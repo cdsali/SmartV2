039af200a8038c36bf381ccec9b76d89523d6a0d
cdsali <cdsali0202@gmail.com>
2024-07-11 21:24:32 +0100

    excude

--- src/components/Home/Exude.js
+++ src/components/Home/Exude.js
@@ -45,8 +45,8 @@ const Section = () => {
         </Col>
         <Col md={6} className="ord2 d-flex flex-column justify-content-center on" >
           <div className="video-wrapper " data-aos="zoom-in">
-            <video autoPlay loop muted playsInline className="video" width="100%">
-              <source src={vid} type="video/webm" />
+          <video autoPlay loop muted playsInline className="video" width="100%">
+              <source src={vid} type="video/mp4" />
               Your browser does not support the video tag.
             </video>
           </div>
